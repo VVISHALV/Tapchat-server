fe68ede146f347fab9233a8f25dc6571c6f6a4be
vignesh14052002 <vignesharivazhagan1409@gmail.com>
2022-08-13 17:00:08 +0530

    fixed cors

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const { Server } = require("socket.io");
+// const { Server } = require("socket.io");
 
-const io = new Server(server);
+// const io = new Server(server);
+const io = require('socket.io')(server, {cors: {origin: "*"}});
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
@@ -19,6 +21,6 @@ io.on('connection', (socket) => {
                   });
 });
 
-server.listen(3000, () => {
+server.listen(3001, () => {
   console.log('listening on *:3000');
 });
